8e046b99b166310390ccf14dd04c2c9bccbbf2d2
Avelardo Vignoto <avelar.vignoto@gmail.com>
2023-03-04 11:48:17 -0300

    Melhoria feita no layout

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.querySelector('#form');
 const taskList = document.querySelector('#task-list');
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
@@ -8,34 +8,34 @@ const renderTaskList = () => {
   tasks.map(task => {
     const taskItem = document.createElement('div');
     taskItem.classList.add('task-item');
-
-    const desc = document.createElement('p');
-    desc.textContent = task.description;
-    desc.classList.add('desc');
+    
+    const description = document.createElement('p');
+    description.textContent = task.description;
+    description.classList.add('description');
     if (task.done) {
-      desc.style.textDecoration = "line-through";
+      description.style.textDecoration = "line-through";
     }
 
-    const done = document.createElement('span');
-    done.textContent = 'Concluir';
-    done.classList.add('done');
-    done.addEventListener('click', e => {
+    const check = document.createElement('span');
+    check.textContent = 'check_circle';
+    check.classList.add('material-symbols-outlined', 'green-check');
+    check.addEventListener('click', e => {
       task.done = true;
       localStorage.setItem('tasks', JSON.stringify(tasks));
       renderTaskList();
     })
 
-    const remover = document.createElement('span');
-    remover.textContent = 'Deletar';
-    remover.classList.add('delete');
-    remover.addEventListener('click', e => {
+    const remove = document.createElement('span');
+    remove.textContent = 'delete';
+    remove.classList.add('material-symbols-outlined', 'red-remove');
+    remove.addEventListener('click', e => {
       const taskIndex = tasks.findIndex(item => item.id === task.id);
       tasks.splice(taskIndex, 1);
       localStorage.setItem('tasks', JSON.stringify(tasks));
       renderTaskList();
     })
 
-    taskItem.append(desc, done, remover);
+    taskItem.append(description, check, remove);
     taskList.append(taskItem);
   })
 
@@ -63,4 +63,4 @@ form.addEventListener('submit', e => {
   renderTaskList();
 })
 
-window.addEventListener('load', loadTasks);
+loadTasks();
